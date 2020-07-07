b4b1eda3d3f86bf9918afd855ae86d67c6feb4bb
Ignacio Petrone <ignacio.petrone@swarovski.com>
2020-07-07 12:54:40 +0200

    Install bcrypt and types. Rename "signUp" mutation with "addUser". Change resolvers and querys. Use "pre" mongoose middleware to hash the incoming password.

--- src/server/resolvers/user.ts
+++ src/server/resolvers/user.ts
@@ -1,15 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import mongoose from 'mongoose';
+import { UserInputError } from 'apollo-server-express';
+import { User } from "../models";
+
 export default {
   Query: {
     users: (root: any, args: any, ctx: any, info: any) => {
-      return;
+      // TODO: auth, projection, pagination
+
+      return User.find({});
     },
     user: (root: any, args: any, ctx: any, info: any) => {
-      return;
+      // TODO: auth, projection, sanitization
+      if(!mongoose.Types.ObjectId.isValid(args.id)) {
+        throw new UserInputError(`${args.id} is not a valir user ID.`)
+      }
+
+      return User.findById(args.id);
     },
   },
   Mutation: {
-    signUp: (root: any, args: any, ctx: any, info: any) => {
-      return;
+    addUser: (root: any, args: any, ctx: any, info: any) => {
+      // TODO: not auth
+
+      // Validation
+      
+
+      return User.create(args);
     }
   }
 }
